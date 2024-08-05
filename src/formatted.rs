f71171a120ec950b3d4d0c33518855a39d52c5f5
TheVeryDarkness <3266343194@qq.com>
2024-08-05 10:40:17 +0800

    Make clippy happy.

--- src/formatted.rs
+++ src/formatted.rs
@@ -3,11 +3,11 @@ use crate::sep_by;
 /// [std::fmt::Display] with given separator.
 pub trait SepBy: IntoIterator {
     /// Create a [sep_by::SepBy] with given separator.
-    fn sep_by<'a>(self, sep: &'a str) -> sep_by::SepBy<'a, Self::IntoIter>;
+    fn sep_by(self, sep: &'_ str) -> sep_by::SepBy<'_, Self::IntoIter>;
 }
 
 impl<I: IntoIterator> SepBy for I {
-    fn sep_by<'a>(self, sep: &'a str) -> sep_by::SepBy<'a, Self::IntoIter> {
+    fn sep_by(self, sep: &'_ str) -> sep_by::SepBy<'_, Self::IntoIter> {
         super::sep_by::SepBy::new(self.into_iter(), sep)
     }
 }
