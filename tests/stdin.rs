81a860c14682ddf9bb1a6526f7c1ee95acc5dc68
TheVeryDarkness <3266343194@qq.com>
2024-08-30 01:07:32 +0800

    Separate some tests into 2 files.

--- tests/stdin.rs
+++ tests/stdin.rs
@@ -1,46 +1,4 @@
 use iof::{read, read_m_n, read_n, try_read, try_read_m_n, try_read_n, Mat};
-use std::{
-    io::Write,
-    process::{Command, Stdio},
-};
-
-fn test_example(name: &str, input: &str) {
-    let mut child = Command::new("cargo")
-        .arg("run")
-        .arg("--example")
-        .arg(name)
-        .stdin(Stdio::piped())
-        .spawn()
-        .unwrap();
-    child
-        .stdin
-        .as_mut()
-        .unwrap()
-        .write_all(input.as_bytes())
-        .unwrap();
-    let status = child.wait().unwrap();
-    assert!(status.success());
-}
-
-#[test]
-fn dot_product() {
-    test_example("dot_product", "3\n1 2 3\n4 5 6\n4 10 18");
-}
-
-/// Reads two lines of strings from input, and checks if they are in reverse order.
-///
-/// ```python
-/// assert read_line() == read_line()[::-1]
-/// ```
-#[test]
-fn string_reverse() {
-    // καλημέραHello,你好！🦀
-    // 🦀！好你,olleHαρέμηλακ
-    test_example(
-        "string_reverse",
-        "καλημέραHello,你好！🦀\n🦀！好你,olleHαρέμηλακ",
-    );
-}
 
 #[test]
 fn read_n_0() {
