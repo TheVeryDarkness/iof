17857f02cc905ee62ab9ba9e2f812cea3ed71905
TheVeryDarkness <3266343194@qq.com>
2024-10-09 10:46:45 +0800

    Clean up codes in benchmarks.

--- benches/show.rs
+++ benches/show.rs
@@ -1,40 +1,31 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use iof::{sep_by, show, unwrap};
+use std::ops::Range;
 
 fn many_integers(c: &mut Criterion) {
+    const INTEGERS: [Range<i64>; 3] = [
+        -0x10000i64..0x10000i64,
+        -0x80000000..-0x7fff0000,
+        0x70000000..0x70010000,
+    ];
     let s = unwrap!(std::fs::read_to_string("benches/integers.txt"));
     c.bench_function("default_separator", |b| {
         b.iter(|| {
             let mut buf = Vec::new();
-            let integers = [
-                -0x10000i64..0x10000i64,
-                -0x80000000..-0x7fff0000,
-                0x70000000..0x70010000,
-            ];
-            show!(sep_by!(integers, "\n", " ") => buf);
+            show!(sep_by!(INTEGERS, "\n", " ") => buf);
             assert_eq!(buf, s.as_bytes());
         })
     })
     .bench_function("longer_separator", |b| {
         b.iter(|| {
             let mut buf = Vec::new();
-            let integers = [
-                -0x10000i64..0x10000i64,
-                -0x80000000..-0x7fff0000,
-                0x70000000..0x70010000,
-            ];
-            show!(sep_by!(integers, "\n\n", " :: ") => buf);
+            show!(sep_by!(INTEGERS, "\n\n", " :: ") => buf);
         })
     })
     .bench_function("char_separator", |b| {
         b.iter(|| {
             let mut buf = Vec::new();
-            let integers = [
-                -0x10000i64..0x10000i64,
-                -0x80000000..-0x7fff0000,
-                0x70000000..0x70010000,
-            ];
-            show!(sep_by!(integers, '\n', ' ') => buf);
+            show!(sep_by!(INTEGERS, '\n', ' ') => buf);
         })
     });
 }
